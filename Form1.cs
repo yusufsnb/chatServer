7deca91bfa52fed2160725e6c57c0331f13d2cbb
yusufsnb <yusufsina.bakan@hotmail.com>
2023-09-14 08:37:31 +0300

    server setup,accept clients

--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,23 @@
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
 namespace chatServer
 {
     public partial class Form1 : Form
     {
+        private string IP;
+        private byte[] _buffer = new byte[1024];//verinin boyutu
+        public List<SocketMsg> _clientSockets { get; set; }
+        List<string> _names = new List<string>();
+        private Socket _serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         public Form1()
         {
             InitializeComponent();
+            findIP();
+            _clientSockets = new List<SocketMsg>();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -19,5 +32,42 @@ namespace chatServer
         private void btnSend_Click(object sender, EventArgs e)
         {
         }
+
+
+        #region Server
+        private void setup()
+        {
+
+            label4.Text = "Server has started...";
+            _serverSocket.Bind(new IPEndPoint(IPAddress.Parse(IP), 100));
+            _serverSocket.Listen(1);
+
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+
+        private void AppceptCallback(IAsyncResult ar)
+        {
+            Socket socket = _serverSocket.EndAccept(ar);
+            _clientSockets.Add(new SocketMsg(socket));
+            listUsers.Items.Add(socket.RemoteEndPoint.ToString());
+
+            socket.BeginReceive(_buffer, 0, _buffer.Length, SocketFlags.None, new AsyncCallback(ReceiveCallback), socket);
+
+            _serverSocket.BeginAccept(new AsyncCallback(AppceptCallback), null);
+        }
+
+       
+
+        //if pc connected to network server IP = ; no-network localhost
+        private void findIP()
+        {
+            IPAddress[] localIP = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress ip in localIP)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                    IP = ip.ToString();
+            }
+        }
+        #endregion
     }
 }
---
+++ SocketMsg.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chatServer
+{
+    public class SocketMsg
+    {
+        public Socket _Socket { get; set; }
+        public string _Name { get; set; }
+        public SocketMsg(Socket socket)
+        {
+            this._Socket = socket;
+        }
+    }
+}
